a3057c7fc9f61f7d8fab8341e398bead8c78b62c
Felix Bünemann <buenemann@louis.info>
2017-11-14 18:50:40 +0100

    Use raw encoding by default to handle binary files

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ function BrotliFilter(inputNode, options) {
                          options.appendSuffix :
                          true);
 
+    // Default file encoding is raw to handle binary files
+    this.inputEncoding = options.inputEncoding || null;
+    this.outputEncoding = options.outputEncoding || null;
+
     if (this.keepUncompressed && !this.appendSuffix) {
         throw new Error('Cannot keep uncompressed files without appending suffix. Filenames would be the same.');
     }
